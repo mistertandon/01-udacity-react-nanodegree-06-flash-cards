fe823bb6efda0d8ee5b96d0dbd0c9d39bd601043
mistertandon <enggparveshtandon@gmail.com>
2018-04-18 00:36:28 +0530

    fix: Fix design for DeckWithNoCard component.

--- components/DeckWithNoCard.js
+++ components/DeckWithNoCard.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { DodgerBlue, White } from '../utils/colors';
 
 export const DeckWithNoCard = () => {
 
   return (
-    <View>
-      <Text>
+    <View
+      style={{ height: 40, justifyContent: 'center', alignItems: 'center', backgroundColor: DodgerBlue, marginTop: 30, marginLeft: 10, marginRight: 10, borderRadius: 15 }}
+    >
+      <Text style={{ color: White }}>
         No Question added to deck yet.
       </Text>
     </View>
